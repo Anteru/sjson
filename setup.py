68ff439eebb39923828bbaca34543c10de97545c
Matthäus G. Chajdas <dev@anteru.net>
2018-05-11 17:49:20 +0200

    Cleanup setup.py.

--- setup.py
+++ setup.py
@@ -1,28 +1,15 @@
 from setuptools import setup, find_packages
 import sys
-from setuptools.command.test import test as TestCommand
 
 import sjson
-
-class Tox(TestCommand):
-    def finalize_options(self):
-        TestCommand.finalize_options(self)
-        self.test_args = []
-        self.test_suite = True
-    def run_tests(self):
-        #import here, cause outside the eggs aren't loaded
-        import tox
-        errcode = tox.cmdline(self.test_args)
-        sys.exit(errcode)
 
 setup(
     name = "SJSON",
     version = sjson.__version__,
     packages = find_packages (exclude=['*.test', 'test.*', '*.test.*']),
 
-    test_suite = 'sjson.test',
-    tests_require=['tox'],
-    cmdclass = {'test' : Tox},
+    setup_requires=['pytest-runner'],
+    test_requires=['pytest'],
 
     install_requires = [],
 
@@ -40,7 +27,8 @@ setup(
         'License :: OSI Approved :: BSD License',
         'Natural Language :: English',
         'Operating System :: OS Independent',
-        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
         'Topic :: Software Development :: Libraries :: Python Modules',
 
     ]
